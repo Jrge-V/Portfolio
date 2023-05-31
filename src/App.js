56f4053b6e254dd2f3a831a6bd15a4b329e13b42
Jrge-V <jorgeverduzco91@gmail.com>
2023-05-31 07:40:58 -0700

    beginning portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/App.css";
+import { MdEmail } from "react-icons/md";
 
-function App() {
+export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="background-image">
+      <div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            className="profile-image"
+            src="/media/Jorge2.jpg"
+            alt="profile"
+          />
+        </div>
+
+        <div className="full-name">Jorge G. Verduzco Espinoza</div>
+
+        <div className="header-image-container">
+          <img
+            className="header-image-static"
+            src="/media/CSUNAlum.png"
+            alt="header"
+          />
+          <img
+            className="header-image-gif"
+            src="/media/CSUNALUMNI.gif"
+            alt="alumni gif"
+          />
+        </div>
+
+        <div className="email-wrapper">
+          <div className="email-container">
+            <MdEmail />
+            example.@email.com
+          </div>
+        </div>
+
+        <div>Github</div>
+        <div>LinkedIn</div>
+        <div>Projects</div>
+      </div>
     </div>
   );
 }
-
-export default App;
