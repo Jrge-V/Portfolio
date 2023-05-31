673266fd012e902af59c54de5f058bab11ef09f4
Jrge-V <jorgeverduzco91@gmail.com>
2023-05-31 10:20:30 -0700

    email, github, linkedin done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
 import "../src/App.css";
 import { MdEmail } from "react-icons/md";
+import clipboardCopy from "clipboard-copy";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
 
 export default function Home() {
+  // function and logic to handle copy text to clipboard
+  const [textToCopy, setTextToCopy] = useState(
+    "jorgeg.verduzcoespinoza@gmail.com"
+  );
+
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyText = () => {
+    clipboardCopy(textToCopy)
+      .then(() => {
+        setIsCopied(true);
+      })
+      .catch((error) => {
+        console.log("Failed to copy", error);
+      });
+  };
+
+  useEffect(() => {
+    let timeoutId;
+
+    if (isCopied) {
+      timeoutId = setTimeout(() => {
+        setIsCopied(false);
+      }, 3000); // Display the pop-up for 3 seconds
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isCopied]);
+
+  const gitRedirect = () => {
+    window.open("https://github.com/Jrge-V", "_blank");
+  };
+
+  const linkedRedirect = () => {
+    window.open("https://www.linkedin.com/in/jorge-verduzco/", "_blank");
+  }
+
+
+
+
+
+
   return (
     <div className="background-image">
       <div>
@@ -10,7 +59,7 @@ export default function Home() {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            paddingTop: "20px",
+            paddingTop: "50px",
           }}
         >
           <img
@@ -36,13 +85,35 @@ export default function Home() {
         </div>
 
         <div className="email-wrapper">
-          <div className="email-container">
+          <button onClick={handleCopyText} className="email-container">
             <MdEmail />
-            example.@email.com
-          </div>
+            jorgeg.verduzcoespinoza@gmail.com
+          </button>
+          {isCopied && (
+            <div className="popup-container">
+              <div className="popup-content">
+                <BsFillEmojiSmileFill />
+                <span>Copied to clipboard</span>
+              </div>
+            </div>
+          )}
         </div>
 
-        <div>Github</div>
+        <div className="github-wrapper">
+        <a href="https://github.com/Jrge-V" target="_blank" rel="noopener noreferrer" className="github-container" onClick={gitRedirect}>
+            <AiFillGithub />
+            github.com/Jrge-V
+          </a>
+        </div>
+
+
+        <div className="linked-wrapper">
+        <a href="https://www.linkedin.com/in/jorge-verduzco/" target="_blank" rel="noopener noreferrer" className="linked-container" onClick={linkedRedirect}>
+            <BsLinkedin />
+            linkedin.com/in/jorge-verduzco
+          </a>
+        </div>
+
         <div>LinkedIn</div>
         <div>Projects</div>
       </div>
